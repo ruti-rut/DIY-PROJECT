49bb4e44d56596a196045b0df131e0b3bd8f468d
yap <yael300500@gmail.com>
2025-10-23 21:41:23 +0300

    project mapper trial

--- src/main/java/com/example/diy/Mapper/ProjectMapper.java
+++ src/main/java/com/example/diy/Mapper/ProjectMapper.java
@@ -1,4 +1,11 @@
 package com.example.diy.Mapper;
 
+import com.example.diy.DTO.ProjectCreateDTO;
+import com.example.diy.model.Project;
+
 public interface ProjectMapper {
+    default ProjectCreateDTO projectToDTO(Project project){
+         ProjectCreateDTO proj=new ProjectCreateDTO();
+        return proj;
+    }
 }
