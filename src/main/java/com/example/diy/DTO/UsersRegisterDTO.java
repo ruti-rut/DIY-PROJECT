8411a2a6f7d08a08c27beb5c032b7892fb8f236f
yap <yael300500@gmail.com>
2025-10-23 20:14:34 +0300

    added getters and setters

--- src/main/java/com/example/diy/DTO/UsersRegisterDTO.java
+++ src/main/java/com/example/diy/DTO/UsersRegisterDTO.java
@@ -5,4 +5,36 @@ public class UsersRegisterDTO {
     private String userName;
     private String password;
     private String mail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }
