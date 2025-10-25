0973e52f7af2cc748b6ae29810f2929200ae5849
ruti-rut <ruti0583210@gmail.com>
2025-10-25 23:19:12 +0300

    add images

--- src/main/java/com/example/diy/Mapper/ProjectMapper.java
+++ src/main/java/com/example/diy/Mapper/ProjectMapper.java
@@ -6,28 +6,29 @@ import com.example.diy.DTO.ProjectListDTO;
 import com.example.diy.model.Project;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
-
-@Mapper(componentModel = "spring", uses = {UsersMapper.class})
+@Mapper(componentModel = "spring", uses = {UsersMapper.class, CategoryMapper.class})
 public interface ProjectMapper {
-        @Mapping(source = "users", target = "usersSimpleDTO")
-        ProjectListDTO projectToListDTO(Project project);
+    @Mapping(source = "users", target = "usersSimpleDTO")
+    ProjectListDTO projectToListDTO(Project project);
 
-        default ProjectCreateDTO ProjectCreateToDTO(Project project, CategoryMapper cm){
-                ProjectCreateDTO projectCreateDTO=new ProjectCreateDTO();
-                projectCreateDTO.setCategory(cm.categoryToDTO(project.getCategory()));
-                projectCreateDTO.setChallenge(project.getChallenge());
-                projectCreateDTO.setSteps(project.getSteps());
-                projectCreateDTO.setTags(project.getTags());
-                projectCreateDTO.setMaterials(project.getMaterials());
-                projectCreateDTO.setTitle(project.getTitle());
-                projectCreateDTO.setAges(project.getAges());
-                projectCreateDTO.setTimePrep(project.getTimePrep());
-                projectCreateDTO.setPicture(project.getPicture());
-                projectCreateDTO.setDescription(project.getDescription());
-                projectCreateDTO.setDraft(project.isDraft());
-                return projectCreateDTO;
-        }
+    @Mapping(source = "users", target = "users")
+    @Mapping(source = "category", target = "category")
+    ProjectDTO projectToDTO(Project project);
+
+    default ProjectCreateDTO ProjectCreateToDTO(Project project, CategoryMapper cm) {
+        ProjectCreateDTO projectCreateDTO = new ProjectCreateDTO();
+        projectCreateDTO.setCategory(cm.categoryToDTO(project.getCategory()));
+        projectCreateDTO.setChallenge(project.getChallenge());
+        projectCreateDTO.setSteps(project.getSteps());
+        projectCreateDTO.setTags(project.getTags());
+        projectCreateDTO.setMaterials(project.getMaterials());
+        projectCreateDTO.setTitle(project.getTitle());
+        projectCreateDTO.setAges(project.getAges());
+        projectCreateDTO.setTimePrep(project.getTimePrep());
+        projectCreateDTO.setPicture(project.getPicture());
+        projectCreateDTO.setDescription(project.getDescription());
+        projectCreateDTO.setDraft(project.isDraft());
+        return projectCreateDTO;
+    }
 }
