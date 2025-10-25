b956f1baaa35d2f3d60ca09ad6a3b3cd31c51480
ruti-rut <ruti0583210@gmail.com>
2025-10-25 23:36:53 +0300

    add images

--- src/main/java/com/example/diy/controller/ProjectController.java
+++ src/main/java/com/example/diy/controller/ProjectController.java
@@ -1,5 +1,37 @@
 package com.example.diy.controller;
 
+import com.example.diy.DTO.ProjectDTO;
+import com.example.diy.Mapper.ProjectMapper;
+import com.example.diy.model.Project;
+import com.example.diy.service.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/recipes")
+@CrossOrigin
 public class ProjectController {
+    private static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "\\images\\";
+    ProjectRepository projectRepository;
+    ProjectMapper projectMapper;
+
+    @Autowired //באופן אוטומטי יצור מחלקה שמממשת את הממשק בצורה מתאימה ויכניס מופע שלה
+    public ProjectController(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    @GetMapping("/getProject/{id}")
+    public ResponseEntity<ProjectDTO> get(@PathVariable long id) throws IOException {
+        Project p = projectRepository.findById(id).get();
+        if (p != null)
+            return new ResponseEntity<>(projectMapper.projectToDTO(p), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 
 }
