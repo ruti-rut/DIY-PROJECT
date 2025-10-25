2622c64256ec60bf9ef46235801ceec2376e70d6
yap <yael300500@gmail.com>
2025-10-25 22:58:37 +0300

    one mapper

--- src/main/java/com/example/diy/Mapper/ProjectMapper.java
+++ src/main/java/com/example/diy/Mapper/ProjectMapper.java
@@ -1,5 +1,6 @@
 package com.example.diy.Mapper;
 
+import com.example.diy.DTO.ProjectCreateDTO;
 import com.example.diy.DTO.ProjectDTO;
 import com.example.diy.DTO.ProjectListDTO;
 import com.example.diy.model.Project;
@@ -13,4 +14,19 @@ import java.io.IOException;
 public interface ProjectMapper {
         @Mapping(source = "users", target = "usersSimpleDTO")
         ProjectListDTO projectToListDTO(Project project);
+        default ProjectCreateDTO ProjectCreateToDTO(Project project, CategoryMapper cm){
+                ProjectCreateDTO projectCreateDTO=new ProjectCreateDTO();
+                projectCreateDTO.setCategory(cm.categoryToDTO(project.getCategory()));
+                projectCreateDTO.setChallenge(project.getChallenge());
+                projectCreateDTO.setSteps(project.getSteps());
+                projectCreateDTO.setTags(project.getTags());
+                projectCreateDTO.setMaterials(project.getMaterials());
+                projectCreateDTO.setTitle(project.getTitle());
+                projectCreateDTO.setAges(project.getAges());
+                projectCreateDTO.setTimePrep(project.getTimePrep());
+                projectCreateDTO.setPicture(project.getPicture());
+                projectCreateDTO.setDescription(project.getDescription());
+                projectCreateDTO.setDraft(project.isDraft());
+                return projectCreateDTO;
+        }
 }
